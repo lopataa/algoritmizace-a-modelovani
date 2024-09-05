dac0ffd5ef958f20aa4903b2c44e6f1a27f1b9ff
lopataa <mkolakovsky@gmail.com>
2024-09-05 11:11:08 +0200

    spiral: fix edge cases

--- spiral/without_recursion.py
+++ spiral/without_recursion.py
@@ -1,20 +1,19 @@
 import turtle
 
 LENGTH =   250
-GAP_SIZE = 50
+GAP_SIZE = 250
 
 turtle.hideturtle()
 turtle.width(5)
 
 def draw_spiral(length, gap_size):
-    if not (length >= gap_size >= 0):
+    if not (length > gap_size > 0):
         raise ValueError("Invalid parameters :(")
 
     if length == LENGTH:
         turtle.forward(length)
 
     while True:
-        print(length, gap_size)
         for i in range(2):
             turtle.right(90)
             turtle.forward(length)
