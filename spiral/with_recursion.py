53e74c3cb5dd1a6a3deb18e0f46606a0bb4c227e
Erik MACÁK <macaer@delta-studenti.cz>
2024-09-05 14:35:31 +0200

    úprava podmínky

--- spiral/with_recursion.py
+++ spiral/with_recursion.py
@@ -7,7 +7,9 @@ turtle.hideturtle()
 turtle.width(5)
 
 def draw_spiral(length, gap_size):
-    if not (length > gap_size >= 0):
+    #if not (length > gap_size >= 0): takhle podmínka nefungovala, nevykreslilo to poslední l a pak to hodilo chybu
+    #if not (length >= gap_size >= 0 and length > 0 and gap_size > 0): tady je to ošetřený a navíc je přidaná podmínka, aby length i gap_size byly větší jak 0
+    if not (length > 0 and 0 < gap_size <= length): #stejná podmínka jako ta řádek nad tím (11) akorát mi chatgpt řeklo, že to je přehlednější :d
        raise ValueError("Invalid parameters :(")
 
     if length == LENGTH:
